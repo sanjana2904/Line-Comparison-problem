081e514b03e39fe30ecf757be310df321bee914f
sanjana k <sanjanarthi@gmail.com>
2021-03-03 18:30:36 +0530

    "Comparing two length"

--- Line Comparison problem/Program.cs
+++ Line Comparison problem/Program.cs
@@ -7,7 +7,7 @@ namespace Line_Comparison_problem
         static void Main(string[] args)
         {
 			int x1 = 1, y1 = 0, x2 = 5, y2 = 0, z1, z2, a;
-			int x3 = 0, y3 = 2, x4 = 0, y4 = 6, z3, z4, b;
+			int x3 = 0, y3 = 2, x4 = 0, y4 = 5, z3, z4, b;
 			z1 = x2 - x1;
 			z2 = y2 - y1;
 			z3 = x4 - x3;
@@ -20,10 +20,17 @@ namespace Line_Comparison_problem
 			{
 				Console.WriteLine("lines are equal");
 			}
+			else if (a > b)
+			{
+				Console.WriteLine("greater");
+			}
 			else
 			{
-				Console.WriteLine("lines are not equal");
+				Console.WriteLine("less");
 			}
+
 		}
-    }
+	}
+
 }
+   
