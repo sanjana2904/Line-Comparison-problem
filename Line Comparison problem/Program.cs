846547e56853187a69f4b473062d1ae694a5422c
sanjana k <sanjanarthi@gmail.com>
2021-03-03 17:59:34 +0530

    Add project files.

---
+++ Line Comparison problem/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Line_Comparison_problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to Line Comparison Computation Program");
+			
+
+		
+		}
+    }
+}
