bf6430e04819ef8288d3dd8a97a8d4496e055fc5
sanjana k <sanjanarthi@gmail.com>
2021-03-03 18:06:44 +0530

    Added length program

--- Line Comparison problem/Program.cs
+++ Line Comparison problem/Program.cs
@@ -6,10 +6,11 @@ namespace Line_Comparison_problem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("welcome to Line Comparison Computation Program");
-			
-
-		
+			int x1 = 1, y1 = 0, x2 = 5, y2 = 0, z1, z2, a;
+			z1 = x2 - x1;
+			z2 = y2 - y1;
+			a = ((z1 * z1) + (z2 * z2));
+			Console.WriteLine("length is:" + Math.Sqrt(a));
 		}
     }
 }
