3b591d21ab0fabfcb7101963db26c04879c4b786
sanjana k <sanjanarthi@gmail.com>
2021-03-04 12:30:50 +0530

    "Added Pow"

--- Line Comparison problem/Program.cs
+++ Line Comparison problem/Program.cs
@@ -6,11 +6,10 @@ namespace Line_Comparison_problem
     {
         static void Main(string[] args)
         {
-			int x1 = 1, y1 = 0, x2 = 5, y2 = 0, z1, z2, a;
-			z1 = x2 - x1;
-			z2 = y2 - y1;
-			a = ((z1 * z1) + (z2 * z2));
-			Console.WriteLine("length is:" + Math.Sqrt(a));
+			int x1 = 1, y1 = 0, x2 = 5, y2 = 0;
+			
+            double len = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+			Console.WriteLine("length is:" + len);
 		}
     }
 }
